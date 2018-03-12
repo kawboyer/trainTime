b05d7ccb4e5b80c77ca5b32f5e10183d795ca52b
kawboyer <kawboyer@gmail.com>
2018-03-11 23:45:37 -0400

    functioning database

--- logic.js
+++ logic.js
@@ -7,11 +7,11 @@ var config = {
     authDomain: "traintime-49b0a.firebaseapp.com",
     databaseURL: "https://traintime-49b0a.firebaseio.com",
     projectId: "traintime-49b0a",
-    storageBucket: "",
+    storageBucket: "traintime-49b0a.appspot.com",
     messagingSenderId: "909287852454"
-    };
+};
 
-    firebase.initializeApp(config);
+firebase.initializeApp(config);
    
 // Create global variables
 
@@ -19,55 +19,54 @@ var trainName = "";
 var destination = "";
 var startTime = "";
 var frequency = 0;
+var nextArrival = "";
+var minAway = 0;
 
-var dataRef = firebase.database()
+var database = firebase.database();
+
+/*
+database.ref().on("value", function(snap) {
+    if(snap.child("highBidder").exists() && snap.child("highPrice").exists()) {
+        highBidder = snap.val().highBidder;
+        highPrice = snap.val().highPrice;
+        $("highestBidder").html(highBidder);
+        $("highestPrice").html("$"+highPrice);
+    };
+});
+*/
 
 // When "submit" button is clicked, the input values are grabbed
 $("#submit").on("click", function() {
     event.preventDefault();
-    var trainName = $("#train-input").val().trim();
-    var destination = $("#destination-input").val().trim();
-    var startTime = $("#first-train-input").val().trim();
-    var frequency = $("#frequency-input").val().trim();
+    trainName = $("#train-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    startTime = $("#first-train-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
 
     console.log(trainName);
     console.log(destination);
     console.log(startTime);
     console.log(frequency);
 
-    return false;
-
-    /*
-    //And pushed into the database
-    dataRef.ref().push({
-        one: trainName,
-        two: destination,
-        three: startTime,
-        four: frequency
+    database.ref().push({
+        trainName:trainName,
+        destination:destination,
+        startTime:startTime,
+        frequency:frequency
     });
-    console.log(trainName);
-    */
+
+    $('form-submit')[0].reset();
+
+    return false;
 });
-/*
-dataRef.ref().on("child_added", function(snap) {
+
+database.ref().on("child_added", function(snap) {
     console.log(snap.val());
-    // console.log(destination);
 
     $(".tbody").append("<tr><td>" + snap.val().trainName + "</td>" +
     "<td>" + snap.val().destination + "</td>" +
-    "<td>" + snap.val().startTime + "</td>" +
-    "<td>" + snap.startTime + "</td>" +
-    "<td>" + frequency + "</td></tr>")
+    "<td>" + snap.val().frequency + " min</td>" +
+    "<td>" + snap.val().nextArrival + "</td>" +
+    "<td>" + snap.val().minAway + " min</td></tr>"
+    );
 });
-*/
-
-
-
-
-
-
-
-
-
-
-
